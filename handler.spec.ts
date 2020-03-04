f3b74590b720a15c578de0aac222847938bf5dd4
Matt Gray <mattgray83@hotmail.co.uk>
2020-03-04 16:01:04 +0000

    Add function to get all sessions

--- handler.spec.ts
+++ handler.spec.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import * as chai from 'chai';
-// @ts-ignore
+
 import {hello, helloAgain} from './handler';
 
 const expect = chai.expect;
@@ -25,6 +25,15 @@ describe("handler", () => {
       })
     });
   });
+
+  //TODO: Mock database and unit test get all sessions
+
+  // describe("Get all sessions", () => {
+  //   it("should return all sessions",  async() => {
+  //     await getAllSessions(null, null, (error: Error, result: any) => {
+  //       expect(error).to.be.null;
+  //       result.body.should.equal('{"message":{"names":["id","presenter","time","title","type","location_id"],"rows":[["48n","Andrei","19:25","Kitchen Session","Round Table","3n"],["49n","Matt","00:00","Matts Test","Practical","1n"],["50n","Sherlock","12:12","Sherlock\'s test","Workshop","6n"],["51n","Matt","15:15","Matt\'s other test","Round Table","11n"]],"status":"SELECT 4"},"input":null}');
+  //     })
+  //   });
+  // });
 });
-
-
--- handler.ts
+++ handler.ts
@@ -1,4 +1,10 @@
 import {APIGatewayEvent, Callback, Context, Handler} from 'aws-lambda';
+import {Client} from "ts-postgres";
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 
 export const hello: Handler = (event: APIGatewayEvent, _context: Context, cb: Callback) => {
   const response = {
@@ -22,4 +28,41 @@ export const helloAgain: Handler = (event: APIGatewayEvent, _context: Context, c
   };
 
   cb(null, response);
+};
+
+export const getAllSessions: Handler = async (event: APIGatewayEvent, _context: Context, cb: Callback) => {
+
+  const client = new Client({
+    host: process.env.PROD_DB_URI,
+    user: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASSWD,
+    port: Number(process.env.PROD_DB_PORT),
+    database: process.env.PROD_DB_NAME
+  });
+
+  await client.connect();
+
+  try {
+    const result = await client.query(`SELECT * FROM sessions`);
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: result,
+        input: event,
+      },function(_key,value) {
+            if (typeof value === 'bigint') {
+              return value.toString() + "n";
+            } else {
+              return value;
+            }
+          }
+      )
+    };
+
+    cb(null, response);
+
+  } finally {
+    await client.end();
+  }
 };
