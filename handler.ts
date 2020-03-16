04a31b8c41626ff733152d3040fa2b2df4b11fed
Matt Gray <mattgray83@hotmail.co.uk>
2020-03-16 15:47:01 +0000

    Fix getSessions in handler

--- handler.ts
+++ handler.ts
@@ -2,13 +2,12 @@ import {APIGatewayEvent, Callback, Context, Handler} from 'aws-lambda';
 import {getAllSessions} from "./src/Sessions/SessionsService/SessionsService";
 import SessionsRepository from "./src/Sessions/SessionsRepository/SessionsRepository";
 
-export const getSessions: Handler = (event: APIGatewayEvent, _context: Context, cb: Callback) => {
+export const getSessions: Handler = async (_event: APIGatewayEvent, _context: Context, cb: Callback) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: getAllSessions(new SessionsRepository),
-      input: event,
-    }),
+    body: JSON.stringify(
+      await getAllSessions(new SessionsRepository)
+    )
   };
 
   cb(null, response);
