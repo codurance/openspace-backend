027182360e5b5f8e20b4417683b4604018291916
Matt Gray <mattgray83@hotmail.co.uk>
2020-03-17 11:56:58 +0000

    Add function for adding sessions

--- handler.ts
+++ handler.ts
@@ -1,5 +1,5 @@
 import {APIGatewayEvent, Callback, Context, Handler} from 'aws-lambda';
-import {getAllSessions} from "./src/Sessions/SessionsService/SessionsService";
+import {addSession, getAllSessions} from "./src/Sessions/SessionsService/SessionsService";
 import SessionsRepository from "./src/Sessions/SessionsRepository/SessionsRepository";
 
 export const getSessions: Handler = async (_event: APIGatewayEvent, _context: Context, cb: Callback) => {
@@ -11,11 +11,25 @@ export const getSessions: Handler = async (_event: APIGatewayEvent, _context: Co
   cb(null, response);
 };
 
-// export const addASession: Handler = async (_event: APIGatewayEvent, _context: Context, cb: Callback) => {
-//   const response = {
-//     statusCode: 200,
-//     body: await addSession()
-//   };
-//
-//   cb(null, response);
-// };
+export const addASession: Handler = async (event: APIGatewayEvent, _context: Context, cb: Callback) => {
+
+  const requestBody = JSON.parse(event.body);
+
+  try {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(await addSession(new SessionsRepository, requestBody))
+    };
+    cb(null, response);
+
+  } catch (e) {
+    const response = {
+      statusCode: 400,
+      //This is only to match exactly the response from the old backend in Spring,
+      //but can probably be changed to something more generic
+      body: new Error("Session type is required")
+    };
+    cb(null, response);
+  }
+
+};
