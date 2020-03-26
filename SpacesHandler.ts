e88bca6a674903875492c46a13828bc667b8de5a
Matt Gray <mattgray83@hotmail.co.uk>
2020-03-26 10:04:59 +0000

    Add edit and delete handlers for spaces

--- SpacesHandler.ts
+++ SpacesHandler.ts
@@ -20,3 +20,23 @@ export const addASpace: Handler = async (event: APIGatewayEvent, _context: Conte
 
   cb(null, response);
 };
+
+export const editASpace: Handler = async (event: APIGatewayEvent, _context: Context, cb: Callback) =>{
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(await spacesRepository.editSpace(parseInt(event.pathParameters.id), event))
+  };
+
+  cb(null, response);
+};
+
+export const deleteASpace: Handler = async (event: APIGatewayEvent, _context: Context, cb: Callback) =>{
+  const result = await spacesRepository.deleteSpace(parseInt(event.pathParameters.id));
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Space " + result[0].id + " has been deleted")
+  };
+
+  cb(null, response);
+};
